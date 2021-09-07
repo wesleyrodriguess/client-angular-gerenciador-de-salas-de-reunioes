6cf121c8f4f392917c9275097153c93ebc693192
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-09-07 18:39:24 -0300

    feat:add config app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CreateRoomComponent } from './create-room/create-room.component';
@@ -11,12 +14,16 @@ import { UpdateRoomComponent } from './update-room/update-room.component';
   declarations: [
     AppComponent,
     CreateRoomComponent,
+    UpdateRoomComponent,
     RoomDetailsComponent,
     RoomListComponent,
-    UpdateRoomComponent
+    RoomListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
